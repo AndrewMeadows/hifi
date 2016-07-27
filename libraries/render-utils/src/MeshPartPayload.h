348e2b7c6393265c4019b72f4a68fa9e7defac71
Andrew Meadows <andrew@highfidelity.io>
2016-07-27 13:52:55 -0700

    don't render raw compound collision geometry

--- libraries/render-utils/src/MeshPartPayload.h
+++ libraries/render-utils/src/MeshPartPayload.h
@@ -24,7 +24,7 @@ class Model;
 class MeshPartPayload {
 public:
     MeshPartPayload() {}
-    MeshPartPayload(const std::shared_ptr<const model::Mesh>& mesh, int partIndex, model::MaterialPointer material, const Transform& transform, const Transform& offsetTransform);
+    MeshPartPayload(const std::shared_ptr<const model::Mesh>& mesh, int partIndex, model::MaterialPointer material);
 
     typedef render::Payload<MeshPartPayload> Payload;
     typedef Payload::DataPointer Pointer;
