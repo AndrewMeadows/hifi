d9fa9ef088883df5c547eb4801e1de86bcb54ba2
Zander Otavka <zotavka@gmail.com>
2016-08-01 17:34:30 -0700

    Make Vec3 constants QVariants and expose converts
    Vec3.fromObject converts a glm::vec3 to a variant, and Vec3.toObject
    does the opposite.

--- libraries/script-engine/src/Vec3.h
+++ libraries/script-engine/src/Vec3.h
@@ -20,28 +20,28 @@
 
 #include "GLMHelpers.h"
 
-/// Scriptable interface a Vec3ernion helper class object. Used exclusively in the JavaScript API
+/// Scriptable interface a Vec3 helper class object. Used exclusively in the JavaScript API
 class Vec3 : public QObject {
     Q_OBJECT
-    Q_PROPERTY(glm::vec3 UNIT_X READ UNIT_X CONSTANT)
-    Q_PROPERTY(glm::vec3 UNIT_Y READ UNIT_Y CONSTANT)
-    Q_PROPERTY(glm::vec3 UNIT_Z READ UNIT_Z CONSTANT)
-    Q_PROPERTY(glm::vec3 UNIT_NEG_X READ UNIT_NEG_X CONSTANT)
-    Q_PROPERTY(glm::vec3 UNIT_NEG_Y READ UNIT_NEG_Y CONSTANT)
-    Q_PROPERTY(glm::vec3 UNIT_NEG_Z READ UNIT_NEG_Z CONSTANT)
-    Q_PROPERTY(glm::vec3 UNIT_XY READ UNIT_XY CONSTANT)
-    Q_PROPERTY(glm::vec3 UNIT_XZ READ UNIT_XZ CONSTANT)
-    Q_PROPERTY(glm::vec3 UNIT_YZ READ UNIT_YZ CONSTANT)
-    Q_PROPERTY(glm::vec3 UNIT_XYZ READ UNIT_XYZ CONSTANT)
-    Q_PROPERTY(glm::vec3 FLOAT_MAX READ FLOAT_MAX CONSTANT)
-    Q_PROPERTY(glm::vec3 FLOAT_MIN READ FLOAT_MIN CONSTANT)
-    Q_PROPERTY(glm::vec3 ZERO READ ZERO CONSTANT)
-    Q_PROPERTY(glm::vec3 ONE READ ONE CONSTANT)
-    Q_PROPERTY(glm::vec3 TWO READ TWO CONSTANT)
-    Q_PROPERTY(glm::vec3 HALF READ HALF CONSTANT)
-    Q_PROPERTY(glm::vec3 RIGHT READ RIGHT CONSTANT)
-    Q_PROPERTY(glm::vec3 UP READ UP CONSTANT)
-    Q_PROPERTY(glm::vec3 FRONT READ FRONT CONSTANT)
+    Q_PROPERTY(QVariant UNIT_X READ UNIT_X CONSTANT)
+    Q_PROPERTY(QVariant UNIT_Y READ UNIT_Y CONSTANT)
+    Q_PROPERTY(QVariant UNIT_Z READ UNIT_Z CONSTANT)
+    Q_PROPERTY(QVariant UNIT_NEG_X READ UNIT_NEG_X CONSTANT)
+    Q_PROPERTY(QVariant UNIT_NEG_Y READ UNIT_NEG_Y CONSTANT)
+    Q_PROPERTY(QVariant UNIT_NEG_Z READ UNIT_NEG_Z CONSTANT)
+    Q_PROPERTY(QVariant UNIT_XY READ UNIT_XY CONSTANT)
+    Q_PROPERTY(QVariant UNIT_XZ READ UNIT_XZ CONSTANT)
+    Q_PROPERTY(QVariant UNIT_YZ READ UNIT_YZ CONSTANT)
+    Q_PROPERTY(QVariant UNIT_XYZ READ UNIT_XYZ CONSTANT)
+    Q_PROPERTY(QVariant FLOAT_MAX READ FLOAT_MAX CONSTANT)
+    Q_PROPERTY(QVariant FLOAT_MIN READ FLOAT_MIN CONSTANT)
+    Q_PROPERTY(QVariant ZERO READ ZERO CONSTANT)
+    Q_PROPERTY(QVariant ONE READ ONE CONSTANT)
+    Q_PROPERTY(QVariant TWO READ TWO CONSTANT)
+    Q_PROPERTY(QVariant HALF READ HALF CONSTANT)
+    Q_PROPERTY(QVariant RIGHT READ RIGHT CONSTANT)
+    Q_PROPERTY(QVariant UP READ UP CONSTANT)
+    Q_PROPERTY(QVariant FRONT READ FRONT CONSTANT)
 
 public slots:
     QVariant reflect(const QVariant& v1, const QVariant& v2);
@@ -65,26 +65,30 @@ public slots:
     QVariant fromSpherical(const QVariant& polar);
     QVariant fromSpherical(float elevation, float azimuth);
 
+    // for QJSEngine so it can access things like MyAvatar.position
+    glm::vec3 fromObject(const QVariant& v) { return vec3FromVariant(v); }
+    QVariant toObject(const glm::vec3& v) { return vec3toVariant(v); }
+
 private:
-    const glm::vec3& UNIT_X() { return Vectors::UNIT_X; }
-    const glm::vec3& UNIT_Y() { return Vectors::UNIT_Y; }
-    const glm::vec3& UNIT_Z() { return Vectors::UNIT_Z; }
-    const glm::vec3& UNIT_NEG_X() { return Vectors::UNIT_NEG_X; }
-    const glm::vec3& UNIT_NEG_Y() { return Vectors::UNIT_NEG_Y; }
-    const glm::vec3& UNIT_NEG_Z() { return Vectors::UNIT_NEG_Z; }
-    const glm::vec3& UNIT_XY() { return Vectors::UNIT_XY; }
-    const glm::vec3& UNIT_XZ() { return Vectors::UNIT_XZ; }
-    const glm::vec3& UNIT_YZ() { return Vectors::UNIT_YZ; }
-    const glm::vec3& UNIT_XYZ() { return Vectors::UNIT_XYZ; }
-    const glm::vec3& FLOAT_MAX() { return Vectors::MAX; }
-    const glm::vec3& FLOAT_MIN() { return Vectors::MIN; }
-    const glm::vec3& ZERO() { return Vectors::ZERO; }
-    const glm::vec3& ONE() { return Vectors::ONE; }
-    const glm::vec3& TWO() { return Vectors::TWO; }
-    const glm::vec3& HALF() { return Vectors::HALF; }
-    const glm::vec3& RIGHT() { return Vectors::RIGHT; }
-    const glm::vec3& UP() { return Vectors::UP; }
-    const glm::vec3& FRONT() { return Vectors::FRONT; }
+    const QVariant& UNIT_X() { return vec3toVariant(Vectors::UNIT_X); }
+    const QVariant& UNIT_Y() { return vec3toVariant(Vectors::UNIT_Y); }
+    const QVariant& UNIT_Z() { return vec3toVariant(Vectors::UNIT_Z); }
+    const QVariant& UNIT_NEG_X() { return vec3toVariant(Vectors::UNIT_NEG_X); }
+    const QVariant& UNIT_NEG_Y() { return vec3toVariant(Vectors::UNIT_NEG_Y); }
+    const QVariant& UNIT_NEG_Z() { return vec3toVariant(Vectors::UNIT_NEG_Z); }
+    const QVariant& UNIT_XY() { return vec3toVariant(Vectors::UNIT_XY); }
+    const QVariant& UNIT_XZ() { return vec3toVariant(Vectors::UNIT_XZ); }
+    const QVariant& UNIT_YZ() { return vec3toVariant(Vectors::UNIT_YZ); }
+    const QVariant& UNIT_XYZ() { return vec3toVariant(Vectors::UNIT_XYZ); }
+    const QVariant& FLOAT_MAX() { return vec3toVariant(Vectors::MAX); }
+    const QVariant& FLOAT_MIN() { return vec3toVariant(Vectors::MIN); }
+    const QVariant& ZERO() { return vec3toVariant(Vectors::ZERO); }
+    const QVariant& ONE() { return vec3toVariant(Vectors::ONE); }
+    const QVariant& TWO() { return vec3toVariant(Vectors::TWO); }
+    const QVariant& HALF() { return vec3toVariant(Vectors::HALF); }
+    const QVariant& RIGHT() { return vec3toVariant(Vectors::RIGHT); }
+    const QVariant& UP() { return vec3toVariant(Vectors::UP); }
+    const QVariant& FRONT() { return vec3toVariant(Vectors::FRONT); }
 };
 
 #endif // hifi_Vec3_h
