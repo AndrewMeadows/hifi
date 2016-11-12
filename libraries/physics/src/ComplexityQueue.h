e2d14432e4b51eb28472d081d41b4cb3b30af519
Andrew Meadows <andrew@highfidelity.io>
2016-11-11 16:26:27 -0800

    woops, forgot to add ComplexityQueue.h

---
+++ libraries/physics/src/ComplexityQueue.h
@@ -0,0 +1,38 @@
+//
+//  ComplexityQueue.h
+//  libraries/physcis/src
+//
+//  Created by Andrew Meadows 2016.12.10
+//  Copyright 2016 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_ComplexityQueue_h
+#define hifi_ComplexityQueue_h
+
+#include <queue>
+
+class ObjectMotionState;
+
+class Complexity {
+public:
+    ObjectMotionState* key;
+    int32_t value;
+
+    class GreaterThan {
+    public:
+        bool operator() (const Complexity& A, const Complexity& B) { return A.value > B.value; }
+    };
+
+    class LessThan {
+    public:
+        bool operator() (const Complexity& A, const Complexity& B) { return A.value < B.value; }
+    };
+};
+
+using ComplexityQueueHighToLow = std::priority_queue<Complexity, std::vector<Complexity>, Complexity::GreaterThan>;
+using ComplexityQueueLowToHigh = std::priority_queue<Complexity, std::vector<Complexity>, Complexity::LessThan>;
+
+#endif // hifi_ComplexityQueue_h
