28970f442736af2705b1c09ff393c8c86ceed7a7
Andrew Meadows <andrew@highfidelity.io>
2017-02-22 11:52:45 -0800

    clarify units of hard coded consts

--- interface/src/avatar/AvatarManager.cpp
+++ interface/src/avatar/AvatarManager.cpp
@@ -599,8 +599,8 @@ RayToAvatarIntersectionResult AvatarManager::findRayIntersection(const PickRay& 
 
 // HACK
 void AvatarManager::setAvatarUpdateTimeBudget(uint32_t timeBudget) {
-    const uint32_t MIN_AVATAR_UPDATE_TIME_BUDGET = 0;
-    const uint32_t MAX_AVATAR_UPDATE_TIME_BUDGET = 11000U;
+    const uint32_t MIN_AVATAR_UPDATE_TIME_BUDGET = 0; // usec
+    const uint32_t MAX_AVATAR_UPDATE_TIME_BUDGET = 11000U; // usec
     _avatarUpdateTimeBudget = (uint64_t)glm::clamp(timeBudget, MIN_AVATAR_UPDATE_TIME_BUDGET, MAX_AVATAR_UPDATE_TIME_BUDGET);
 }
 
