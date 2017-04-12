8d7644dd91921ede49a5fdfbe257099b58197870
Andrew Meadows <andrew@highfidelity.io>
2017-04-12 12:20:12 -0700

    remove last qApp usage from Avatar class

--- interface/src/Application.h
+++ interface/src/Application.h
@@ -270,7 +270,7 @@ public:
     int getMaxOctreePacketsPerSecond() const;
 
     render::ScenePointer getMain3DScene() override { return _main3DScene; }
-    render::ScenePointer getMain3DScene() const { return _main3DScene; }
+    const render::ScenePointer& getMain3DScene() const { return _main3DScene; }
     render::EnginePointer getRenderEngine() override { return _renderEngine; }
     gpu::ContextPointer getGPUContext() const { return _gpuContext; }
 
@@ -280,7 +280,7 @@ public:
 
     float getAvatarSimrate() const { return _avatarSimCounter.rate(); }
     float getAverageSimsPerSecond() const { return _simCounter.rate(); }
-    
+
     void takeSnapshot(bool notify, bool includeAnimated = false, float aspectRatio = 0.0f);
     void shareSnapshot(const QString& filename, const QUrl& href = QUrl(""));
 
