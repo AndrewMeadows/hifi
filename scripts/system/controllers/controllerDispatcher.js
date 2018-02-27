3764d5586eddd179b155a95b532e2d900a0c9861
Andrew Meadows <andrew@highfidelity.io>
2018-02-27 15:38:34 -0800

    fix bug in controllerDispatcher.js

--- scripts/system/controllers/controllerDispatcher.js
+++ scripts/system/controllers/controllerDispatcher.js
@@ -378,8 +378,8 @@ Script.include("/~/system/libraries/controllerDispatcherUtils.js");
         };
 
         this.setBlacklist = function() {
-            Picks.setIgnoreItems(_this.leftControllerRayPick, this.blacklist);
-            Picks.setIgnoreItems(_this.rightControllerRayPick, this.blacklist);
+            Picks.setIgnoreItems(_this.leftPointer, this.blacklist);
+            Picks.setIgnoreItems(_this.rightPointer, this.blacklist);
         };
 
         var MAPPING_NAME = "com.highfidelity.controllerDispatcher";
