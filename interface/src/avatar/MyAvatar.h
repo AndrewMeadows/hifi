c7e4ce1af75734ee4739167fd683ca2c738442c1
Andrew Meadows <andrew@highfidelity.io>
2018-12-15 06:24:30 -0800

    MyAvatar.updateAvatarEntity(id, data) works in theory

--- interface/src/avatar/MyAvatar.h
+++ interface/src/avatar/MyAvatar.h
@@ -1176,6 +1176,7 @@ public:
 
     AvatarEntityMap getAvatarEntityData() const override;
     void setAvatarEntityData(const AvatarEntityMap& avatarEntityData) override;
+    void updateAvatarEntity(const QUuid& entityID, const QByteArray& entityData) override;
     void avatarEntityDataToJson(QJsonObject& root) const override;
 
 public slots:
